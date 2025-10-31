6bcd1ffbdc2243a1ca3272dba6e50941ddd666f7
Devprince2025 <ishimweprince999@gmail.com>
2025-11-01 01:31:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,6 @@ import Students from './pages/Students';
 import GenericPage from './pages/GenericPage';
 import Admission from './pages/Admission';
 import Reports from './pages/Reports';
-function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        {/* Fixed positioned components */}
-        <Sidebar />
-        <TopBar />
-        
-        {/* Main content with proper spacing */}
-        <main className="ml-64 pt-16 min-h-screen">
-          <div className="p-6">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/students" element={<Students />} />
-              <Route path="/admission" element={<Admission />} />
-              <Route path="/rooms" element={<GenericPage title="Amacumbi" />} />
-              <Route path="/porogaramu" element={<GenericPage title="Porogaramu" />} />
-              <Route path="/reports" element={<Reports />} />
-              <Route path="/imyicarire" element={<GenericPage title="Imyicarire" />} />
-              <Route path="/ibikorwa" element={<GenericPage title="Ibikorwa" />} />
-              <Route path="/amatangazo" element={<GenericPage title="Amatangazo" />} />
-              <Route path="/abakoresha" element={<GenericPage title="Abakoresha" />} />
-              <Route path="/igenamiterere" element={<GenericPage title="Igenamiterere" />} />
-              <Route path="/amabwiriza" element={<GenericPage title="Amabwiriza" />} />
-              <Route path="/shyiraho-porogaramu" element={<GenericPage title="Shyiraho Porogaramu" />} />
-            </Routes>
-          </div>
-        </main>
-      </div>
-    </Router>
 import Program from './pages/Program';
 import Refectory from './pages/Refectory';
 
